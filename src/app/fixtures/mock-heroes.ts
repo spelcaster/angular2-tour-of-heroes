22f70bf9161038c9c2ab48fc077b9bb266666577
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2016-06-12 18:07:49 -0300

    Hero fixtures
    - A few heroes to be used in the sample application;

---
+++ src/app/fixtures/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from "../classes/hero/hero";
+
+// mocked heroes
+export var HERO_LIST: Hero[] = [
+    { "id": 11, "name": "License-less Rider" },
+    { "id": 12, "name": "Caped Baldy" },
+    { "id": 13, "name": "Demon Cyborg" },
+    { "id": 14, "name": "Tornado of Terror" },
+    { "id": 15, "name": "Silver Fang" },
+    { "id": 16, "name": "All Might" },
+    { "id": 17, "name": "Eraserhead" },
+    { "id": 18, "name": "Deku" },
+    { "id": 19, "name": "Uravity" },
+    { "id": 20, "name": "Tenya" }
+];
+
