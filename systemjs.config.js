bfa4cfb52b205d18bd1da26e8e9696601ba00a04
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2016-06-19 21:04:03 -0300

    Updating systemjs.config.js
    - Removing @ngrx/* and dependencies;
    - Update angular2 bundles;
    - Angular2 new route component couldn't be loaded automatically, so it
      was manually configured;

--- systemjs.config.js
+++ systemjs.config.js
@@ -12,19 +12,7 @@
 
         // angular2
         '@angular': 'node_modules/@angular',
-
-        // @ngrx/core
-        '@ngrx/core': 'node_modules/@ngrx/core',
-
-        // @ngrx/router
-        '@ngrx/router': 'node_modules/@ngrx/router',
-
-        // @ngrx/router dependencies
-        'path-to-regexp': 'node_modules/path-to-regexp',
-        'isarray': 'node_modules/isarray',
-        'query-string': 'node_modules/query-string',
-        'strict-uri-encode': 'node_modules/strict-uri-encode',
-        'object-assign': 'node_modules/object-assign'
+        '@angular/router': 'node_modules/@angular/router'
     };
 
     // packages tells the System loader how to load when no filename and/or
@@ -42,37 +30,7 @@
             main: 'index.js',
             defaultExtension: 'js'
         },
-
-        // @ngrx/core package
-        '@ngrx/core': {
-            main: 'index.js',
-            defaultExtension: 'js'
-        },
-
-        // @ngrx/router package
-        '@ngrx/router': {
-            main: 'index.js',
-            defaultExtension: 'js'
-        },
-
-        // @ngrx/router dependencies
-        'path-to-regexp': {
-            main: 'index.js',
-            defaultExtension: 'js'
-        },
-        'isarray': {
-            main: 'index.js',
-            defaultExtension: 'js'
-        },
-        'query-string': {
-            main: 'index.js',
-            defaultExtension: 'js'
-        },
-        'strict-uri-encode': {
-            main: 'index.js',
-            defaultExtension: 'js'
-        },
-        'object-assign': {
+        '@angular/router': {
             main: 'index.js',
             defaultExtension: 'js'
         }
@@ -96,10 +54,10 @@
         };
     }
 
-    // individual files (~300 requests)
+    // bundled (~40 requests)
     function packUmd(pkgName) {
         packages['@angular/' + pkgName] = {
-            main: pkgName + '.umd.js',
+            main: 'bundles/' + pkgName + '.umd.js',
             defaultExtension: 'js'
         };
     }
